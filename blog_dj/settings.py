dd302e92a3272b3d73c855a8dace0333cf4c247c
Razin <tanvirrazin@gmail.com>
2014-11-25 21:15:54 +0600

    completed serializer tutorial

--- blog_dj/settings.py
+++ blog_dj/settings.py
@@ -63,8 +63,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'blog_dj',
-        'USER': 'razin',
-        'PASSWORD': 'bangla'
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,21 +3,26 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
-    pk = serializers.Field()
-    title = serializers.CharField(required=False, max_length=100)
-    code = serializers.CharField(widget=widgets.Textarea, max_length=100000)
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class SnippetSerializer(serializers.ModelSerializer):
 
-    def restore_object(self, attrs, instance=None):
-        if instance:
-            instance.title = attrs.get('title', instance.title)
-            instance.code = attrs.get('code', instance.code)
-            instance.linenos = attrs.get('linenos', instance.linenos)
-            instance.language = attrs.get('language', instance.language)
-            instance.style = attrs.get('style', instance.style)
-            return instance
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
 
-        return Snippet(**attrs)
+    # pk = serializers.Field()
+    # title = serializers.CharField(required=False, max_length=100)
+    # code = serializers.CharField(widget=widgets.Textarea, max_length=100000)
+    # linenos = serializers.BooleanField(required=False)
+    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+    #
+    # def restore_object(self, attrs, instance=None):
+    #     if instance:
+    #         instance.title = attrs.get('title', instance.title)
+    #         instance.code = attrs.get('code', instance.code)
+    #         instance.linenos = attrs.get('linenos', instance.linenos)
+    #         instance.language = attrs.get('language', instance.language)
+    #         instance.style = attrs.get('style', instance.style)
+    #         return instance
+    #
+    #     return Snippet(**attrs)
