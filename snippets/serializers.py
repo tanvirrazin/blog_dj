2a9b995d9036183d9d22c941f6c0c9c3342c08a0
Razin <tanvirrazin@gmail.com>
2014-11-27 20:31:43 +0600

    completed 4th chapter of DRF

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,28 +1,19 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField(read_only=True)
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner',)
 
-    # pk = serializers.Field()
-    # title = serializers.CharField(required=False, max_length=100)
-    # code = serializers.CharField(widget=widgets.Textarea, max_length=100000)
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    #
-    # def restore_object(self, attrs, instance=None):
-    #     if instance:
-    #         instance.title = attrs.get('title', instance.title)
-    #         instance.code = attrs.get('code', instance.code)
-    #         instance.linenos = attrs.get('linenos', instance.linenos)
-    #         instance.language = attrs.get('language', instance.language)
-    #         instance.style = attrs.get('style', instance.style)
-    #         return instance
-    #
-    #     return Snippet(**attrs)
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets',)
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -6,6 +6,13 @@ from snippets import views
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+]
